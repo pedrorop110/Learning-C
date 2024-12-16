21bd795c4d61026960fc44fc4db619d003098602
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-16 20:25:25 -0300

    Add files via upload

---
+++ beecrown, exerc1004.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+ 
+int A;
+int B;
+int PROD;
+
+scanf("%d",&A);
+scanf("%d",&B);
+
+PROD=A*B;
+
+printf("PROD = %d\n",PROD);
+ 
+    return 0;
+}
