cfc04ba2faac5168148192e8904183d0ce55ee66
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-16 20:11:16 -0300

    Update beecrown, exerc1002.c

--- beecrown, exerc1002.c
+++ beecrown, exerc1002.c
@@ -2,6 +2,15 @@
 #include <math.h>
 #define pi 3.14159
 
+/* Link da questão: https://resources.beecrowd.com/repository/UOJ_1002_en.html
+
+Aqui ele pede para você calcular a area de um circulo
+
+*/
+
+
+
+
 int main() 
 {
  double R,A;
