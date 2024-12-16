395e86a8f435d77a74dbe6e7dbde69e2e39fd3dc
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-16 20:12:44 -0300

    Update beecrown, exerc 1003.c

--- beecrown, exerc 1003.c
+++ beecrown, exerc 1003.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
- 
+
+/* Link da questão: https://resources.beecrowd.com/repository/UOJ_1003_en.html
+
+Aqui ele pede para voce fazer uma calculadora simples de SOMA.
+
+*/
+
 int main()
 {
 
