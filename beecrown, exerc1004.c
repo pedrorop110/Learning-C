6a91e15b4a24537aa46accddf60ca5aac11ce7ce
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-16 20:26:50 -0300

    Update beecrown, exerc1004.c

--- beecrown, exerc1004.c
+++ beecrown, exerc1004.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+/* Link da questão: https://resources.beecrowd.com/repository/UOJ_1004_en.html
+
+A questão pede para fazer uma calculadora simples de MULTIPLACAÇÃO
+*/
+
 int main() {
  
 int A;
