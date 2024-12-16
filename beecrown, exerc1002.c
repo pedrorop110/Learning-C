9360412989b7b06f341519755c819be2d44de48e
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-16 19:52:19 -0300

    Add files via upload

---
+++ beecrown, exerc1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14159
+
+int main() 
+{
+ double R,A;
+ double n =pi;
+ 
+ scanf("%lf",&R);
+ 
+ A=n*pow(R,2);
+ 
+ printf("A=%.4lf\n", A);
+ 
+    return 0;
+}
