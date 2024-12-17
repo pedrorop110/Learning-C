b15c0e5acbf0ac2a5cf36c700f9a49b412cafcda
pedrorop110 <65739565+pedrorop110@users.noreply.github.com>
2024-12-17 15:10:53 -0300

    Add files via upload

---
+++ beecrown, exerc1005.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+float A = 3.5;
+float B = 7.5;
+float MEDIA;
+
+scanf("%f",&A);
+scanf("%f",&B);
+
+MEDIA = (A * 3.5+ B * 7.5) / (3.5+7.5);
+
+printf("MEDIA = %.5f\n",MEDIA);
+
+
+    return 0;
+}
